f0b8840a778476740ba2bdf6456df11a291ecd6b
KalybosPro <kalyboskokou1@gmail.com>
2026-04-02 16:10:51 +0000

    uneccessary files deleted

--- main_production.dart
+++
@@ -1,7 +0,0 @@
-import 'package:chapchap/app/app.dart';
-import 'package:chapchap/bootstrap.dart';
-import 'package:env/env.dart';
-
-void main() {
-  bootstrap(() => App(),env: AppFlavor.production().getEnv);
-}
